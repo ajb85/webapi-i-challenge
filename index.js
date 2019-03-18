66f666bf08d026ff0231f3efc720fd2a85ab13be
ajb85 <ajbrush85@gmail.com>
2019-03-18 16:54:00 -0400

    Get request for /api/users added

--- index.js
+++ index.js
@@ -22,7 +22,17 @@ server.post("/api/users", (req, res) => {
   }
 });
 
-server.get("/api/users", (req, res) => {});
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." })
+    );
+});
 
 server.get("/api/:id", (req, res) => {});
 
