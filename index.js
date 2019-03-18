47d772c1cf938e21a11014893947d58b47216f6d
ajb85 <ajbrush85@gmail.com>
2019-03-18 16:48:23 -0400

    post request added for /api/users

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+const server = express();
+server.use(express.json());
+
+server.post("/api/users", (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    db.insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: "The users information could not be retrieved." })
+      );
+  }
+});
+
+server.get("/api/users", (req, res) => {});
+
+server.get("/api/:id", (req, res) => {});
+
+server.delete("/api/users/:id", (req, res) => {});
+
+server.put("/api/users/:id", (req, res) => {});
+
+server.listen((port = 5000), () => {
+  console.log(`\nListening on ${port}`);
+});
